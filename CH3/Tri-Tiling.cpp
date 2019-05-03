7a3331526e01ac65b57c77f16c6ad338ed2acb65
viewv <zxn@zxnnet.top>
2019-05-03 21:25:57 +0800

    add  explain

--- CH3/Tri-Tiling.cpp
+++ CH3/Tri-Tiling.cpp
@@ -12,6 +12,9 @@ int main(int argc, char const *argv[])
     dp[2] = 3;
     for (int i = 4; i <= 30; i += 2)
     {
+        // dp[n] = 3dp[n-2] + 2dp[n-4] + 2dp[n-6] + ... + 2dp[0]
+        // dp[n-2] = 3dp[n-4] + 2dp[n-6] + 2dp[n-8] + ... + 2dp[0]
+        // dp[n] - dp[n-2] => dp[n] = 4dp[n - 2] - dp[n - 4]
         dp[i] = 4 * dp[i - 2] - dp[i - 4];
     }
     while (cin >> n && n != -1)
